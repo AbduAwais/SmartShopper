afd8fc17139f9b1176ce7b5c1e2888eb09a2cd6e
abdurrahmanahmed <eaa23aaa@students.eaaa.dk>
2025-08-03 00:17:47 +0200

    Kamera burde virker nu. Ikke på simuleringens delen der viser den baere en hvid skærm hvor man kan indtaste barcode numre

--- SmartShopperApp/ScannerPage.xaml.cs
+++ SmartShopperApp/ScannerPage.xaml.cs
@@ -10,6 +10,34 @@ public partial class ScannerPage : ContentPage
         InitializeComponent();
     }
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (DeviceHelper.IsSimulator())
+        {
+            resultLabel.Text = "Camera not available in simulator.\nType a barcode or click Simulate.";
+            FakeScanButton.IsVisible = true;
+        }
+        else
+        {
+            // Dynamically create the camera view
+            var cameraView = new CameraBarcodeReaderView
+            {
+                IsDetecting = true,
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill
+            };
+
+            cameraView.BarcodesDetected += OnBarcodesDetected;
+
+            // Add as first child in the grid
+            (Content as Grid)?.Children.Insert(0, cameraView);
+
+            FakeScanButton.IsVisible = false;
+        }
+    }
+
     private void OnBarcodesDetected(object sender, BarcodeDetectionEventArgs e)
     {
         MainThread.BeginInvokeOnMainThread(() =>
@@ -21,4 +49,17 @@ public partial class ScannerPage : ContentPage
             }
         });
     }
-}
+
+    private async void OnFakeScanClicked(object sender, EventArgs e)
+    {
+        string fakeBarcode = await DisplayPromptAsync(
+            "Simulate Scan",
+            "Enter a barcode value to simulate:",
+            initialValue: "1234567890123");
+
+        if (!string.IsNullOrWhiteSpace(fakeBarcode))
+        {
+            resultLabel.Text = $"Simulated Scan: {fakeBarcode}";
+        }
+    }
+} // 👈 make sure this closing brace is there
