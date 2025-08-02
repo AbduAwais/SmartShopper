d7d359878128faa13312255e3d4bc5bc106bd92c
abdurrahmanahmed <eaa23aaa@students.eaaa.dk>
2025-08-02 20:28:05 +0200

    First commit template is done

---
+++ SmartShopperApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartShopperApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SmartShopperApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartShopperApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
